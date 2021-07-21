510b81bb7fcab158b30e2ac95b4a6d2250f371a2
liaoguiqiu <156798895@qq.com>
2021-07-21 10:49:22 +0200

    cleaned the trainer of CE net

--- train_lumen_sheath_segmentation/contour_sheath_CEnet_with_D.py
+++ train_lumen_sheath_segmentation/contour_sheath_CEnet_with_D.py
@@ -1,5 +1,6 @@
 # the run py script for sheal and contour detection, 5th October 2020 update
 # this uses the encodinhg tranform to use discriminator
+# update on 26th July 
 import torch.nn as nn
 import torch.utils.data
 from torch.autograd import Variable
@@ -11,7 +12,7 @@ import arg_parse
 import cv2
 import numpy
 import rendering
-from dataTool. generator_contour import Generator_Contour,Save_Contour_pkl,Communicate
+from dataTool.generator_contour import Generator_Contour,Save_Contour_pkl,Communicate
 
 import os
 from dataset_sheath import myDataloader,Batch_size,Resample_size, Path_length
@@ -19,17 +20,17 @@ from deploy.basic_trans import Basic_oper
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Switch control for the Visdom or Not
-Visdom_flag  = True 
-OLG_flag = False
-Hybrid_OLG = False
-validation_flag = False
-infinite_save_id =0
-Display_fig_flag = True
-Continue_flag = True
+Visdom_flag  = True  # the flag of using the visdom or not
+OLG_flag = False    # flag of training with on line generating or not
+Hybrid_OLG = False  # whether  mix with online generated images and real images for training
+validation_flag = False  # flag to stop the gradient, and, testing mode which will calculate matrics for validation
+Display_fig_flag = True  #  display and save result or not 
+Continue_flag = True   # if not true, it start from scratch again 
+infinite_save_id =0 # use this method so that the index of the image will not start from 0 again when switch the folder    
+
 if Visdom_flag == True:
     from analy_visdom import VisdomLinePlotter
     plotter = VisdomLinePlotter(env_name='path finding training Plots')
-
 
 pth_save_dir = "../out/sheathCGAN_coordinates3/"
 #pth_save_dir = "../out/deep_layers/"
@@ -37,149 +38,16 @@ pth_save_dir = "../out/sheathCGAN_coordinates3/"
 if not os.path.exists(pth_save_dir):
     os.makedirs(pth_save_dir)
 from scipy import signal 
-Matrix_dir =  "../dataset/CostMatrix/1/"
-Save_pic_dir = '../DeepPathFinding/out/'
-opt = arg_parse.opt
-opt.cuda = True
-# check the cuda device 
-print(torch.cuda.current_device())
-print(torch.cuda.device(0))
-print(torch.cuda.device_count())
-print(torch.cuda.get_device_name(0))
-print(torch.cuda.is_available())
-dataroot = "../dataset/CostMatrix/"
-
-torch.set_num_threads(2)
-######################################################################
-# Data
-# ----
-# 
-# In this tutorial we will use the `Celeb-A Faces
-# dataset <http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html>`__ which can
-# be downloaded at the linked site, or in `Google
-# Drive <https://drive.google.com/drive/folders/0B7EVK8r0v71pTUZsaXdaSnZBZzg>`__.
-# The dataset will download as a file named *img_align_celeba.zip*. Once
-# downloaded, create a directory named *celeba* and extract the zip file
-# into that directory. Then, set the *dataroot* input for this notebook to
-# the *celeba* directory you just created. The resulting directory
-# structure should be:
-# 
-# ::
-# 
-#    /path/to/celeba
-#        -> img_align_celeba  
-#            -> 188242.jpg
-#            -> 173822.jpg
-#            -> 284702.jpg
-#            -> 537394.jpg
-#               ...
-# 
-# This is an important step because we will be using the ImageFolder
-# dataset class, which requires there to be subdirectories in the
-# dataset’s root folder. Now, we can create the dataset, create the
-# dataloader, set the device to run on, and finally visualize some of the
-# training data.
-# 
-
-# We can use an image folder dataset the way we have it setup.
-# Create the dataset
- 
-nz = int(arg_parse.opt.nz) # number of latent variables
-ngf = int(arg_parse.opt.ngf) # inside generator
-ndf = int(arg_parse.opt.ndf) # inside discriminator
-nc = 3 # channels
-
-# custom weights initialization called on netG and netD
+# weight init
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         m.weight.data.normal_(0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
-
-
-#netG = gan_body._netG()
-#netG.apply(weights_init)
-#if opt.netG != '':
-#    netG.load_state_dict(torch.load(opt.netG))
-#print(netG)
-
-
-#netD = gan_body._netD()
-#Guiqui 8 layers version
-#netD = gan_body._netD_8()
-
-#Guiqiu Resnet version
-#netD = layer_body_sheath._netD_8_multiscal_fusion300_layer()
-gancreator = cGAN_build2.CGAN_creator() # the Cgan for the segmentation 
-GANmodel= gancreator.creat_cgan()  #  G and D are created here 
-#netD = gan_body._netD_Resnet()
-
-
-
-
-#netD.apply(weights_init)
-GANmodel.netD.apply(weights_init)
-GANmodel.netG.apply(weights_init)
-if Continue_flag == True:
-    #netD.load_state_dict(torch.load(opt.netD))
-    GANmodel.netG.load_state_dict(torch.load(pth_save_dir+'cGANG_epoch_4.pth'))
-    GANmodel.netD.load_state_dict(torch.load(pth_save_dir+'cGAND_epoch_4.pth'))
-    #GANmodel.netG.side_branch1. load_state_dict(torch.load(pth_save_dir+'cGANG_branch1_epoch_1.pth'))
-
-    #torch.save(GANmodel.netG.side_branch1.  state_dict(), pth_save_dir+ "cGANG_branch1_epoch_"+str(epoch)+".pth")
-
-
-print(GANmodel.netD)
-print(GANmodel.netG)
- # no longer use the mine nets 
-
-
-criterion = nn.L1Loss()
-criterion2  = nn.CrossEntropyLoss()
-#criterion = nn.BCELoss()
-
-input = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
-noise = torch.FloatTensor(opt.batchSize, nz, 1, 1)
-fixed_noise = torch.FloatTensor(opt.batchSize, nz, 1, 1).normal_(0, 1)
-label = torch.FloatTensor(opt.batchSize)
-real_label = 1
-fake_label = 0
-
-if opt.cuda:
-    print("CUDA TRUE")
-    GANmodel.netD.cuda()
-    GANmodel.netG.cuda()
-
-    #netD.cuda()
-    #netG.cuda()
-    criterion.cuda()
-    input, label = input.cuda(), label.cuda()
-    noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
-
-
-fixed_noise = Variable(fixed_noise)
-
-# setup optimizer
-#optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-#optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999),weight_decay =2e-4 )
-#optimizerD = optim.SGD(netD.parameters(), lr=opt.lr,momentum= 0.9, weight_decay =2e-4 )
-
-
-#saved_stastics = MY_ANALYSIS()
-#saved_stastics=saved_stastics.read_my_signal_results()
-#saved_stastics.display()
-
-read_id =0
-
-epoch=0
-#transform = BaseTransform(  Resample_size,(104/256.0, 117/256.0, 123/256.0))
-#transform = BaseTransform(  Resample_size,[104])  #gray scale data
-iteration_num =0
-mydata_loader1 = myDataloader (Batch_size,Resample_size,Path_length,validation = validation_flag,OLG=OLG_flag)
-mydata_loader2 = myDataloader (Batch_size,Resample_size,Path_length,validation = validation_flag,OLG=False)
-switcher =0
+        m.bias.data.fill_(0)      
+    pass
+# 3 functions to drae the results in real time
 def draw_coordinates_color(img1,vy,color):
         
         if color ==0:
@@ -288,10 +156,63 @@ def display_prediction(read_id,mydata_loader,save_out,hot,hot_real): # display i
                         str(mydata_loader.save_id) +".jpg",circular2 )  
     cv2.imshow('Deeplearning color',color2.astype(numpy.uint8)) 
     cv2.imshow('  color real',color_real.astype(numpy.uint8)) 
+
+
+Matrix_dir =  "../dataset/CostMatrix/1/"
+Save_pic_dir = '../DeepPathFinding/out/'
+opt = arg_parse.opt
+opt.cuda = True
+# check the cuda device 
+print(torch.cuda.current_device())
+print(torch.cuda.device(0))
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0))
+print(torch.cuda.is_available())
+dataroot = "../dataset/CostMatrix/"
+torch.set_num_threads(2)
+ 
   
+#Guiqui 8 layers version
+#netD = gan_body._netD_8()
 
+#Guiqiu Resnet version
+#netD = layer_body_sheath._netD_8_multiscal_fusion300_layer()
 
+gancreator = cGAN_build2.CGAN_creator() # the Cgan for the segmentation 
+GANmodel= gancreator.creat_cgan()  #  G and D are created here 
+#netD = gan_body._netD_Resnet()
 
+#netD.apply(weights_init)
+GANmodel.netD.apply(weights_init)
+GANmodel.netG.apply(weights_init)
+if Continue_flag == True:
+    #netD.load_state_dict(torch.load(opt.netD))
+    GANmodel.netG.load_state_dict(torch.load(pth_save_dir+'cGANG_epoch_4.pth'))
+    GANmodel.netD.load_state_dict(torch.load(pth_save_dir+'cGAND_epoch_4.pth'))
+    #GANmodel.netG.side_branch1. load_state_dict(torch.load(pth_save_dir+'cGANG_branch1_epoch_1.pth'))
+
+print(GANmodel.netD)
+print(GANmodel.netG)
+ # no longer use the mine nets 
+  
+real_label = 1
+fake_label = 0
+
+if opt.cuda:
+    print("CUDA TRUE")
+    GANmodel.netD.cuda()
+    GANmodel.netG.cuda()
+     
+
+read_id =0
+
+epoch=0
+#transform = BaseTransform(  Resample_size,(104/256.0, 117/256.0, 123/256.0))
+#transform = BaseTransform(  Resample_size,[104])  #gray scale data
+iteration_num =0
+mydata_loader1 = myDataloader (Batch_size,Resample_size,Path_length,validation = validation_flag,OLG=OLG_flag)
+mydata_loader2 = myDataloader (Batch_size,Resample_size,Path_length,validation = validation_flag,OLG=False)
+switcher =0
 
 
 while(1):
@@ -375,32 +296,7 @@ while(1):
             GANmodel.optimize_parameters()   # calculate loss functions, get gradients, update network weights
 
          
-        #outputall = netD(inputv)
-        #outputall =  outputall[:,:,0,:]
-        #output = outputall[0].view(Batch_size,netD.layer_num,Path_length).squeeze(1)
-        #output1 = outputall[1].view(Batch_size,netD.layer_num,Path_length).squeeze(1)
-        #output2 = outputall[2].view(Batch_size,netD.layer_num,Path_length).squeeze(1)
-        ##output = outputall[0]  
-        ##output1 = outputall[1] 
-        ##output2 = outputall[2]  
-        ##output
-
-
-        #netG.zero_grad()
-        #errD_real = criterion(Fake, real)
-        ##errD_real1 = criterion(output1, labelv)
-        ##errD_real2 = criterion(output2, labelv)
-        ##errD_real_fuse = 1.0*(errD_real+  0.1*errD_real1 +  0.1*errD_real2)
-
-        ##errD_real1.backward()errD_real = criterion(output, labelv)
-        ##errD_real1 = criterion(output1, labelv)
-        ##errD_real2 = criterion(output22, labelv)
-        ##errD_real_fuse =   0.1*errD_real1 +  0.1*errD_real2
-
-        #errD_real.backward()
-
-        
-        #errD_real.backward()
+      
  
         if validation_flag ==False:
             D_x = GANmodel.loss_D.data.mean()
@@ -432,19 +328,11 @@ while(1):
                 plotter.plot( 'l1', 'l1', 'l1', iteration_num, GANmodel.displayloss1.cpu().detach().numpy())
                 plotter.plot( 'l2', 'l2', 'l2', iteration_num, GANmodel.displayloss2.cpu().detach().numpy())
                 plotter.plot( 'l3', 'l3', 'l3', iteration_num, GANmodel.displayloss3.cpu().detach().numpy())
-
-                #plotter.plot( 'cLOSS1', 'cLOSS1', 'cLOSS1', iteration_num, D_x1.cpu().detach().numpy())
-                #plotter.plot( 'cLOSS12', 'cLOSS2', 'cLOSS2', iteration_num, D_x2.cpu().detach().numpy())
-                #plotter.plot( 'cLOSS_f', 'cLOSSf', 'cLOSSf', iteration_num, D_xf.cpu().detach().numpy())
+                 
         if read_id % 1 == 0 and Display_fig_flag== True:
             #vutils.save_image(real_cpu,
             #        '%s/real_samples.png' % opt.outf,
             #        normalize=True)
-            #netG.eval()
-            #fake = netG(fixed_noise)
-            #cv2.imwrite(Save_pic_dir  + str(i) +".jpg", mat)
-            #show the result
-
 
             gray2  =   realA[0,0,:,:].cpu().detach().numpy()*104+104
             show1 = gray2.astype(float)
@@ -456,23 +344,18 @@ while(1):
 
             oneHot =  GANmodel.fake_B_1_hot[0,:,:,:].cpu().detach().numpy() 
 
-            
             hot  = numpy.zeros((oneHot.shape[1],oneHot.shape[2],3))
             hot[:,:,0]  =  oneHot [0,:,:]
             hot[:,:,1]  =  oneHot [1,:,:]
             hot[:,:,2]  =  oneHot [2,:,:]
 
             oneHot_real =  GANmodel.real_B_one_hot[0,:,:,:].cpu().detach().numpy() 
-
-            
+          
             hot_real  = numpy.zeros((oneHot.shape[1],oneHot.shape[2],3))
             hot_real[:,:,0]  =  oneHot_real [0,:,:]
             hot_real[:,:,1]  =  oneHot_real [1,:,:]
             hot_real[:,:,2]  =  oneHot_real [2,:,:]
           
-
-
-
             #oneHot  = GANmodel.fake_B_1_hot[0,:,:,:]
             #oneHot = oneHot.view(Path_length,Path_length,3)
             #oneHot  =  oneHot.cpu().detach().numpy()
@@ -483,7 +366,6 @@ while(1):
             #    color1 = draw_coordinates_color(color1,path2[i],i)
                  
             saveout  = GANmodel.fake_B
-            
             show2 =  saveout[0,:,:,:].cpu().detach().numpy()*255 
 
             
